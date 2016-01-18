d2cc2c8902dc7935269fc0ba9c18561c0e7bd76a
John Bywater <john.bywater@appropriatesoftware.net>
2016-01-18 14:31:35 +0000

    Adjusted Python stored event repository to drop references to discarded objects (so they don't accumulate).

--- eventsourcing/infrastructure/stored_events/python_objects_stored_events.py
+++ eventsourcing/infrastructure/stored_events/python_objects_stored_events.py
@@ -18,18 +18,31 @@ class PythonObjectsStoredEventRepository(StoredEventRepository):
         stored_entity_id = stored_event.stored_entity_id
         topic = stored_event.event_topic
 
-        # Index by event ID.
-        self._by_id[event_id] = stored_event
+        if topic.endswith('Discarded'):
 
-        # Index by entity ID.
-        if stored_entity_id not in self._by_stored_entity_id:
-            self._by_stored_entity_id[stored_entity_id] = []
-        self._by_stored_entity_id[stored_entity_id].append(stored_event)
+            self.remove_entity(stored_entity_id)
 
-        # Index by event topic.
-        if topic not in self._by_topic:
-            self._by_topic[topic] = []
-        self._by_topic[topic].append(stored_event)
+        else:
+            # Index by event ID.
+            self._by_id[event_id] = stored_event
+
+            # Index by entity ID.
+            if stored_entity_id not in self._by_stored_entity_id:
+                self._by_stored_entity_id[stored_entity_id] = []
+            self._by_stored_entity_id[stored_entity_id].append(stored_event)
+
+            # Index by event topic.
+            if topic not in self._by_topic:
+                self._by_topic[topic] = []
+            self._by_topic[topic].append(stored_event)
+
+    def remove_entity(self, stored_entity_id):
+        if stored_entity_id in self._by_stored_entity_id:
+            for stored_event in self._by_stored_entity_id.pop(stored_entity_id):
+                del(self._by_id[stored_event.event_id])
+                if stored_event.event_topic in self._by_topic:
+                    self._by_topic[stored_event.event_topic].remove(stored_event)
+
 
     def __getitem__(self, event_id):
         return self._by_id[event_id]
