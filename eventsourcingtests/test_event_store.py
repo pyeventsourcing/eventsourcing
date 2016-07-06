b6757b8a4f1e220c7d5f7cb06ddbc65745ccf755
John Bywater <john.bywater@appropriatesoftware.net>
2016-07-06 20:35:30 +0100

    Changed event store test to use the event store to get events as well as storing them (was using the repo directly to get events).

--- eventsourcingtests/test_event_store.py
+++ eventsourcingtests/test_event_store.py
@@ -11,21 +11,29 @@ class TestEventStore(unittest.TestCase):
         event_store = EventStore(stored_event_repo=repo)
 
         # Check there are zero stored events in the repo.
-        entity_events = repo.get_entity_events(stored_entity_id='Example::entity1')
+        entity_events = event_store.get_entity_events(stored_entity_id='Example::entity1')
+        entity_events = list(entity_events)
         self.assertEqual(0, len(entity_events))
 
         # Store a domain event.
         event1 = Example.Created(entity_id='entity1', a=1, b=2)
         event_store.append(event1)
 
-        # Check there is one stored event in the repo.
-        entity_events = repo.get_entity_events(stored_entity_id='Example::entity1')
+        # Check there is one event in the event store.
+        entity_events = event_store.get_entity_events(stored_entity_id='Example::entity1')
+        entity_events = list(entity_events)
         self.assertEqual(1, len(entity_events))
 
         # Store another domain event.
         event1 = Example.Created(entity_id='entity1', a=1, b=2)
         event_store.append(event1)
 
-        # Check there are two stored events in the repo.
-        entity_events = repo.get_entity_events(stored_entity_id='Example::entity1')
+        # Check there are two events in the event store.
+        entity_events = event_store.get_entity_events(stored_entity_id='Example::entity1')
+        entity_events = list(entity_events)
+        self.assertEqual(2, len(entity_events))
+
+        # Check there are two events in the event store.
+        entity_events = event_store.get_entity_events(stored_entity_id='Example::entity1')
+        entity_events = list(entity_events)
         self.assertEqual(2, len(entity_events))
