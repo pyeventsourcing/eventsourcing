24e00ebe0ab5930d00bbd00c9a29dec3c6cc8bb1
John Bywater <john.bywater@appropriatesoftware.net>
2016-07-11 18:16:10 +0100

    Changed Cassandra connection values to use environment variables.

--- eventsourcing/application/with_cassandra.py
+++ eventsourcing/application/with_cassandra.py
@@ -1,12 +1,14 @@
 from eventsourcing.application.base import EventSourcingApplication
 from eventsourcing.infrastructure.stored_events.cassandra_stored_events import setup_cassandra_connection, \
-    get_cassandra_setup_params, CassandraStoredEventRepository, DEFAULT_CASSANDRA_KEYSPACE
+    get_cassandra_setup_params, CassandraStoredEventRepository, DEFAULT_CASSANDRA_KEYSPACE, DEFAULT_CASSANDRA_HOSTS, \
+    DEFAULT_CASSANDRA_CONSISTENCY_LEVEL, DEFAULT_CASSANDRA_PORT, DEFAULT_CASSANDRA_PROTOCOL_VERSION
 
 
 class EventSourcingWithCassandra(EventSourcingApplication):
 
-    def __init__(self, hosts=('localhost',), consistency='LOCAL_QUORUM', default_keyspace=DEFAULT_CASSANDRA_KEYSPACE,
-                 port=9042, protocol_version=2, username=None, password=None, **kwargs):
+    def __init__(self, hosts=DEFAULT_CASSANDRA_HOSTS, consistency=DEFAULT_CASSANDRA_CONSISTENCY_LEVEL,
+                 default_keyspace=DEFAULT_CASSANDRA_KEYSPACE, port=DEFAULT_CASSANDRA_PORT,
+                 protocol_version=DEFAULT_CASSANDRA_PROTOCOL_VERSION, username=None, password=None, **kwargs):
         self.setup_cassandra_connection(hosts, consistency, default_keyspace, port, protocol_version, username,
                                         password)
         super(EventSourcingWithCassandra, self).__init__(**kwargs)
