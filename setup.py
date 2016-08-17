46fbab8a995615fcd74ba0620d8ebb42fe7bdb8d
John Bywater <john.bywater@appropriatesoftware.net>
2016-08-17 22:23:40 +0100

    Increased version number to 1.0.10

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='eventsourcing',
-    version='1.0.9',
+    version='1.0.10',
     description='Event sourcing in Python',
     author='John Bywater',
     author_email='john.bywater@appropriatesoftware.net',
