d5c7de3a92dcd369ba9254d26a3faeeae159b54b
John Bywater <john.bywater@appropriatesoftware.net>
2016-10-14 12:57:23 +0100

    Removed versions from dependencies in setup.py.

--- setup.py
+++ setup.py
@@ -22,10 +22,10 @@ setup(
     ] + install_requires_singledispatch,
     extras_require={
         'cassandra': [
-            'cassandra-driver==3.7.0',
+            'cassandra-driver',
         ],
         'test': [
-            'cassandra-driver==3.7.0',
+            'cassandra-driver',
             'mock',
             # 'numpy',
             'PyCrypto',
