29f069be6d853260aa088f882cc99768f60e9cfd
John Bywater <john.bywater@appropriatesoftware.net>
2016-11-06 13:36:04 +0000

    Added sequence reader, and moved methods depending on the repo from sequence entity to the sequence reader.

--- eventsourcing/tests/test_sequence.py
+++ eventsourcing/tests/test_sequence.py
@@ -23,31 +23,58 @@ class Sequence(EventSourcedEntity):
     def name(self):
         return self.id
 
-    def append(self, item):
-        stored_entity_id = self._repo.event_player.make_stored_entity_id(self.name)
-        last_event = self._repo.event_store.get_most_recent_event(stored_entity_id)
-        next_version = last_event.entity_version + 1
-        event = Sequence.Appended(
-            entity_id=self.name,
-            entity_version=next_version,
-            item=item,
-        )
-        publish(event)
+
+def start_sequence(name):
+    event = Sequence.Started(entity_id=name)
+    entity = Sequence.mutate(event=event)
+    publish(event)
+    return entity
+
+
+def append_item_to_sequence(name, item, repo):
+    stored_entity_id = repo.event_player.make_stored_entity_id(name)
+    last_event = repo.event_store.get_most_recent_event(stored_entity_id)
+    next_version = last_event.entity_version + 1
+    event = Sequence.Appended(
+        entity_id=name,
+        entity_version=next_version,
+        item=item,
+    )
+    publish(event)
+
+
+class SequenceRepository(EntityRepository):
+    pass
+
+
+class SequenceRepo(EventSourcedRepository, SequenceRepository):
+    domain_class = Sequence
+
+    def get_entity(self, entity_id, until=None):
+        # Replay domain events.
+        return self.event_player.replay_events(entity_id, limit=1)
+
+
+class SequenceReader(object):
+    def __init__(self, sequence, event_player):
+        assert isinstance(sequence, Sequence), sequence
+        assert isinstance(event_player, EventPlayer), event_player
+        self.sequence = sequence
+        self.event_player = event_player
 
     def __getitem__(self, item):
         assert isinstance(item, (six.integer_types, slice))
-        assert isinstance(self._repo.event_player, EventPlayer)
-        stored_entity_id = self._repo.event_player.make_stored_entity_id(self.name)
+        stored_entity_id = self.event_player.make_stored_entity_id(self.sequence.name)
         if isinstance(item, six.integer_types):
             if item < 0:
                 raise IndexError('Negative indexes are not supported')
             try:
-                entity_version = self._repo.event_store.get_entity_version(stored_entity_id, item)
+                entity_version = self.event_player.event_store.get_entity_version(stored_entity_id, item)
             except EntityVersionDoesNotExist:
                 raise IndexError("Entity version not found for index: {}".format(item))
             assert isinstance(entity_version, EntityVersion)
             event_id = entity_version.event_id
-            events = self._repo.event_player.event_store.get_entity_events(stored_entity_id, after=event_id, limit=1)
+            events = self.event_player.event_store.get_entity_events(stored_entity_id, after=event_id, limit=1)
             events = list(events)
             if len(events) == 0:
                 raise IndexError("Entity version not found for index: {}".format(item))
@@ -68,58 +95,33 @@ class Sequence(EventSourcedEntity):
             else:
                 limit = item.stop - item.start
 
-            start_version = self._repo.event_store.get_entity_version(stored_entity_id, start_index)
+            start_version = self.event_player.event_store.get_entity_version(stored_entity_id, start_index)
             start_event_id = start_version.event_id
 
-            events = self._repo.event_player.event_store.get_entity_events(stored_entity_id,
-                                                                           after=start_event_id,
-                                                                           limit=limit)
+            events = self.event_player.event_store.get_entity_events(stored_entity_id,
+                                                                                  after=start_event_id,
+                                                                                  limit=limit)
             items = [e.item for e in events]
             return items
 
     def __len__(self):
-        stored_entity_id = self._repo.event_player.make_stored_entity_id(self.name)
-        event = self._repo.event_store.get_most_recent_event(stored_entity_id)
+        event = self.event_player.get_most_recent_event(self.sequence.name)
         return event.entity_version
 
 
-def start_sequence(name, repo):
-    event = Sequence.Started(entity_id=name)
-    entity = Sequence.mutate(event=event)
-    publish(event)
-    entity._repo = repo
-    return entity
-
-
-class SequenceRepository(EntityRepository):
-    pass
-
-
-class SequenceRepo(EventSourcedRepository, SequenceRepository):
-    domain_class = Sequence
-
-    def get_entity(self, entity_id, until=None):
-        # Replay domain events.
-        sequence = self.event_player.replay_events(entity_id, limit=1)
-        if sequence is not None:
-            sequence._repo = self
-        return sequence
-
-
 class SequenceTestCase(AppishTestCase):
-
     def test(self):
         repo = SequenceRepo(self.event_store)
 
         # Start a new sequence.
         name = 'sequence1'
-        sequence = start_sequence(name, repo)
+        sequence = start_sequence(name)
         self.assertIsInstance(sequence, Sequence)
 
         # Append some items.
-        sequence.append('item1')
-        sequence.append('item2')
-        sequence.append('item3')
+        append_item_to_sequence(name, 'item1', repo)
+        append_item_to_sequence(name, 'item2', repo)
+        append_item_to_sequence(name, 'item3', repo)
 
         # Check the sequence in the repo.
         sequence = repo[name]
@@ -128,52 +130,52 @@ class SequenceTestCase(AppishTestCase):
         self.assertEqual(sequence.version, 1)
 
         # Check the sequence indexing.
-        self.assertEqual(sequence[0], 'item1')
-        self.assertEqual(sequence[1], 'item2')
-        self.assertEqual(sequence[2], 'item3')
+        reader = SequenceReader(sequence, repo.event_player)
+        self.assertEqual(reader[0], 'item1')
+        self.assertEqual(reader[1], 'item2')
+        self.assertEqual(reader[2], 'item3')
 
         # Check slices also work.
-        self.assertEqual(sequence[0:], ['item1', 'item2', 'item3'])
-        self.assertEqual(sequence[0:3], ['item1', 'item2', 'item3'])
-        self.assertEqual(sequence[0:2], ['item1', 'item2'])
-        self.assertEqual(sequence[0:1], ['item1'])
-        self.assertEqual(sequence[1:], ['item2', 'item3'])
-        self.assertEqual(sequence[1:3], ['item2', 'item3'])
-        self.assertEqual(sequence[1:2], ['item2'])
-        self.assertEqual(sequence[1:1], [])
-        self.assertEqual(sequence[2:], ['item3'])
-        self.assertEqual(sequence[2:3], ['item3'])
-        self.assertEqual(sequence[3:], [])
-        self.assertEqual(sequence[3:3], [])
-        self.assertEqual(sequence[0:300], ['item1', 'item2', 'item3'])
-        self.assertEqual(sequence[2:1], [])
+        self.assertEqual(reader[0:], ['item1', 'item2', 'item3'])
+        self.assertEqual(reader[0:3], ['item1', 'item2', 'item3'])
+        self.assertEqual(reader[0:2], ['item1', 'item2'])
+        self.assertEqual(reader[0:1], ['item1'])
+        self.assertEqual(reader[1:], ['item2', 'item3'])
+        self.assertEqual(reader[1:3], ['item2', 'item3'])
+        self.assertEqual(reader[1:2], ['item2'])
+        self.assertEqual(reader[1:1], [])
+        self.assertEqual(reader[2:], ['item3'])
+        self.assertEqual(reader[2:3], ['item3'])
+        self.assertEqual(reader[3:], [])
+        self.assertEqual(reader[3:3], [])
+        self.assertEqual(reader[0:300], ['item1', 'item2', 'item3'])
+        self.assertEqual(reader[2:1], [])
 
         # Check iterator.
-        for i, item in enumerate(sequence):
-            self.assertEqual(item, 'item{}'.format(i+1))
+        for i, item in enumerate(reader):
+            self.assertEqual(item, 'item{}'.format(i + 1))
 
         # Check len.
-        self.assertEqual(len(sequence), 3)
+        self.assertEqual(len(reader), 3)
 
         # Check index errors.
         with self.assertRaises(IndexError):
-            sequence[3]
+            reader[3]
 
         with self.assertRaises(IndexError):
-            sequence[4]
+            reader[4]
 
         with self.assertRaises(IndexError):
-            sequence[-1]
+            reader[-1]
 
         with self.assertRaises(IndexError):
-            sequence[-2:-1]
+            reader[-2:-1]
 
         with self.assertRaises(IndexError):
-            sequence[-1:0]
+            reader[-1:0]
 
         with self.assertRaises(IndexError):
-            sequence[0:-1]
-
+            reader[0:-1]
 
 
 class TestPythonObjectsSequence(PythonObjectsRepoTestCase, SequenceTestCase):
