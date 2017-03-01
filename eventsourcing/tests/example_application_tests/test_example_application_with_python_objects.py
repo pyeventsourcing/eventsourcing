54986ffa162ccaae827099a0a2f9f79acf38fc80
John Bywater <john.bywater@appropriatesoftware.net>
2017-03-01 17:42:30 +0000

    Removed old stuff.

--- eventsourcing/tests/example_application_tests/test_example_application_with_python_objects.py
+++
@@ -1,8 +0,0 @@
-# from eventsourcing.application.example.with_pythonobjects import ExampleApplicationWithPythonObjects
-# from eventsourcing.tests.example_application_tests.base import ExampleApplicationTestCase
-#
-#
-# class TestApplicationWithPythonObjects(ExampleApplicationTestCase):
-#
-#     def create_app(self):
-#         return ExampleApplicationWithPythonObjects()
