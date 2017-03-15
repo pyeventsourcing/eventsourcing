7f10fa661b9317e7b0ca15dd15b3a0928fdb365d
John Bywater <john.bywater@appropriatesoftware.net>
2017-03-15 17:52:42 +0000

    Added files to git (missing after folder rename).

---
+++ eventsourcing/tests/sequenced_item_tests/test_python_objects_stored_event_repository.py
@@ -0,0 +1,36 @@
+from eventsourcing.infrastructure.storedevents.pythonobjectsrepo import PythonObjectsStoredEventRepository
+from eventsourcing.tests.datastore_tests.base import AbstractDatastoreTestCase
+from eventsourcing.tests.sequenced_item_tests.base import CombinedSequencedItemRepositoryTestCase, \
+    SimpleStoredEventIteratorTestCase, StoredEventRepositoryTestCase, ThreadedStoredEventIteratorTestCase
+
+
+class PythonObjectsDatastoreTestCase(AbstractDatastoreTestCase):
+    def construct_datastore(self):
+        return None
+
+
+class PythonObjectsRepoTestCase(PythonObjectsDatastoreTestCase, CombinedSequencedItemRepositoryTestCase):
+
+    def construct_integer_sequenced_item_repository(self):
+        return PythonObjectsStoredEventRepository(
+            always_write_entity_version=True,
+            always_check_expected_version=True,
+        )
+
+
+class TestPythonObjectsStoredEventRepository(PythonObjectsRepoTestCase, StoredEventRepositoryTestCase):
+    pass
+
+
+class TestSimpleStoredEventIteratorWithPythonObjects(PythonObjectsRepoTestCase, SimpleStoredEventIteratorTestCase):
+    pass
+
+
+class TestThreadedStoredEventIteratorWithPythonObjects(PythonObjectsRepoTestCase, ThreadedStoredEventIteratorTestCase):
+    pass
+
+
+# Todo: Revisit this, but with threading rather than multiprocessing because data is stored in process.
+# class TestConcurrentStoredEventRepositoryWithPythonObjects(PythonObjectsRepoTestCase,
+# OptimisticConcurrencyControlTestCase):
+#     pass
