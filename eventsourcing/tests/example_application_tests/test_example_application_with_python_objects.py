1e6d783bec700742574dc9a48168a9413cd46aab
John Bywater <john.bywater@appropriatesoftware.net>
2017-03-18 15:13:34 +0000

    Suspended test (no longer needed).

--- eventsourcing/tests/example_application_tests/test_example_application_with_python_objects.py
+++ eventsourcing/tests/example_application_tests/test_example_application_with_python_objects.py
@@ -1,14 +1,14 @@
-from eventsourcing.tests.example_application_tests.base import ExampleApplicationTestCase
-from eventsourcing.tests.sequenced_item_tests.test_python_objects_stored_event_repository import \
-    PythonObjectsRepoTestCase
-
-
-class TestExampleApplicationWithPythonObjects(PythonObjectsRepoTestCase, ExampleApplicationTestCase):
-    pass
-
-
-class TestExampleApplicationWithPythonObjectsUseCache(PythonObjectsRepoTestCase, ExampleApplicationTestCase):
-    def construct_application(self):
-        app = super(TestExampleApplicationWithPythonObjectsUseCache, self).construct_application()
-        app.example_repo._use_cache = True
-        return app
+# from eventsourcing.tests.example_application_tests.base import ExampleApplicationTestCase
+# from eventsourcing.tests.sequenced_item_tests.test_python_objects_stored_event_repository import \
+#     PythonObjectsRepoTestCase
+#
+#
+# class TestExampleApplicationWithPythonObjects(PythonObjectsRepoTestCase, ExampleApplicationTestCase):
+#     pass
+#
+#
+# class TestExampleApplicationWithPythonObjectsUseCache(PythonObjectsRepoTestCase, ExampleApplicationTestCase):
+#     def construct_application(self):
+#         app = super(TestExampleApplicationWithPythonObjectsUseCache, self).construct_application()
+#         app.example_repo._use_cache = True
+#         return app
