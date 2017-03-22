dbb4326b615e5a2626d7b7b70cea10cacf5f0b03
John Bywater <john.bywater@appropriatesoftware.net>
2017-03-22 08:44:16 +0000

    Removed old code.

--- eventsourcing/application/policies.py
+++ eventsourcing/application/policies.py
@@ -22,26 +22,6 @@ class PersistencePolicy(object):
         unsubscribe(self.store_event, self.is_event)
 
 
-class TimestampedEntityPersistencePolicy(object):
-    """
-    Persists timestamped entity events, whenever they are published.
-    """
-    def __init__(self, event_store):
-        assert isinstance(event_store, AbstractEventStore)
-        self.event_store = event_store
-        subscribe(self.store_event, self.is_timestamped_entity_event)
-
-    @staticmethod
-    def is_timestamped_entity_event(event):
-        return isinstance(event, TimestampedEntityEvent)
-
-    def store_event(self, event):
-        self.event_store.append(event)
-
-    def close(self):
-        unsubscribe(self.store_event, self.is_timestamped_entity_event)
-
-
 class CombinedPersistencePolicy(object):
     """
     Persists both timestamped and versioned entity events, whenever they are published.
