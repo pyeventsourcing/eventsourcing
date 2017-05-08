eda72462a8e431512af45f7a0807d9b49e2fd414
John Bywater <john.bywater@appropriatesoftware.net>
2017-05-08 15:20:35 +0100

    Changed Cassandra active record strategy.
    It now reflects off its sequenced item
    type to get field names of active record,
    so it is similar to the SQLAlchemy active
    record strategy, and satisfies the general
    claims made in the documentation about
    being able to change the persistence model.

--- eventsourcing/infrastructure/cassandra/activerecords.py
+++ eventsourcing/infrastructure/cassandra/activerecords.py
@@ -1,9 +1,10 @@
 import six
 from cassandra.cqlengine.functions import Token
-from cassandra.cqlengine.models import Model, columns
+from cassandra.cqlengine.models import columns
 from cassandra.cqlengine.query import BatchQuery, LWTException
 
 from eventsourcing.infrastructure.activerecord import AbstractActiveRecordStrategy
+from eventsourcing.infrastructure.cassandra.datastore import ActiveRecord
 
 
 class CassandraActiveRecordStrategy(AbstractActiveRecordStrategy):
@@ -13,12 +14,8 @@ class CassandraActiveRecordStrategy(AbstractActiveRecordStrategy):
                 b = BatchQuery()
                 for i in sequenced_item_or_items:
                     assert isinstance(i, self.sequenced_item_class), (type(i), self.sequenced_item_class)
-                    self.active_record_class.batch(b).if_not_exists().create(
-                        s=i.sequence_id,
-                        p=i.position,
-                        t=i.topic,
-                        d=i.data,
-                    )
+                    kwargs = {n: getattr(i, n) for n in self.field_names}
+                    self.active_record_class.batch(b).if_not_exists().create(**kwargs)
                 try:
                     b.execute()
                 except LWTException as e:
@@ -31,7 +28,11 @@ class CassandraActiveRecordStrategy(AbstractActiveRecordStrategy):
                 self.raise_sequenced_item_error(sequenced_item_or_items, e)
 
     def get_item(self, sequence_id, eq):
-        query = self.filter(s=sequence_id, p__eq=eq)
+        kwargs = {
+            self.field_names.sequence_id: sequence_id,
+            '{}__eq'.format(self.field_names.position): eq
+        }
+        query = self.filter(**kwargs)
         items = six.moves.map(self.from_active_record, query)
         items = list(items)
         try:
@@ -46,19 +47,25 @@ class CassandraActiveRecordStrategy(AbstractActiveRecordStrategy):
         assert not (gte and gt)
         assert not (lte and lt)
 
-        query = self.filter(s=sequence_id)
+        kwargs = {self.field_names.sequence_id: sequence_id}
+        query = self.filter(**kwargs)
 
         if query_ascending:
-            query = query.order_by('p')
+            query = query.order_by(self.field_names.position)
 
+        position_name = self.field_names.position
         if gt is not None:
-            query = query.filter(p__gt=gt)
+            kwargs = {'{}__gt'.format(position_name): gt}
+            query = query.filter(**kwargs)
         if gte is not None:
-            query = query.filter(p__gte=gte)
+            kwargs = {'{}__gte'.format(position_name): gte}
+            query = query.filter(**kwargs)
         if lt is not None:
-            query = query.filter(p__lt=lt)
+            kwargs = {'{}__lt'.format(position_name): lt}
+            query = query.filter(**kwargs)
         if lte is not None:
-            query = query.filter(p__lte=lte)
+            kwargs = {'{}__lte'.format(position_name): lte}
+            query = query.filter(**kwargs)
 
         if limit is not None:
             query = query.limit(limit)
@@ -94,103 +101,87 @@ class CassandraActiveRecordStrategy(AbstractActiveRecordStrategy):
         Returns an active record instance, from given sequenced item.
         """
         assert isinstance(sequenced_item, self.sequenced_item_class), (type(sequenced_item), self.sequenced_item_class)
-        return self.active_record_class(
-            s=sequenced_item.sequence_id,
-            p=sequenced_item.position,
-            t=sequenced_item.topic,
-            d=sequenced_item.data
-        )
+        kwargs = {n: getattr(sequenced_item, n) for n in self.field_names}
+        return self.active_record_class(**kwargs)
 
     def from_active_record(self, active_record):
         """
         Returns a sequenced item instance, from given active record.
         """
-        return self.sequenced_item_class(
-            sequence_id=active_record.s,
-            position=active_record.p,
-            topic=active_record.t,
-            data=active_record.d,
-        )
+        kwargs = {n: getattr(active_record, n) for n in self.field_names}
+        return self.sequenced_item_class(**kwargs)
 
     def filter(self, *args, **kwargs):
         return self.active_record_class.objects.filter(*args, **kwargs)
 
 
-class IntegerSequencedItemRecord(Model):
+class IntegerSequencedItemRecord(ActiveRecord):
     """Stores integer-sequenced items in Cassandra."""
-
+    __table_name__ = 'integer_sequenced_items'
     _if_not_exists = True
 
-    __table_name__ = 'integer_sequenced_items'
-
     # Sequence ID (e.g. an entity or aggregate ID).
-    s = columns.UUID(partition_key=True)
+    sequence_id = columns.UUID(partition_key=True)
 
     # Position (index) of item in sequence.
-    p = columns.BigInt(clustering_order='DESC', primary_key=True)
+    position = columns.BigInt(clustering_order='DESC', primary_key=True)
 
     # Topic of the item (e.g. path to domain event class).
-    t = columns.Text(required=True)
+    topic = columns.Text(required=True)
 
     # State of the item (serialized dict, possibly encrypted).
-    d = columns.Text(required=True)
+    data = columns.Text(required=True)
 
 
-class TimestampSequencedItemRecord(Model):
+class TimestampSequencedItemRecord(ActiveRecord):
     """Stores timestamp-sequenced items in Cassandra."""
-
-    _if_not_exists = True
-
     __table_name__ = 'timestamp_sequenced_items'
+    _if_not_exists = True
 
     # Sequence ID (e.g. an entity or aggregate ID).
-    s = columns.UUID(partition_key=True)
+    sequence_id = columns.UUID(partition_key=True)
 
     # Position (in time) of item in sequence.
-    p = columns.Double(clustering_order='DESC', primary_key=True)
+    position = columns.Double(clustering_order='DESC', primary_key=True)
 
     # Topic of the item (e.g. path to domain event class).
-    t = columns.Text(required=True)
+    topic = columns.Text(required=True)
 
     # State of the item (serialized dict, possibly encrypted).
-    d = columns.Text(required=True)
+    data = columns.Text(required=True)
 
 
-class CqlTimeuuidSequencedItem(Model):
+class CqlTimeuuidSequencedItem(ActiveRecord):
     """Stores timeuuid-sequenced items in Cassandra."""
-
-    _if_not_exists = True
-
     __table_name__ = 'timeuuid_sequenced_items'
+    _if_not_exists = True
 
     # Sequence UUID (e.g. an entity or aggregate ID).
-    s = columns.UUID(partition_key=True)
+    sequence_id = columns.UUID(partition_key=True)
 
     # Position (in time) of item in sequence.
-    p = columns.TimeUUID(clustering_order='DESC', primary_key=True)
+    position = columns.TimeUUID(clustering_order='DESC', primary_key=True)
 
     # Topic of the item (e.g. path to domain event class).
-    t = columns.Text(required=True)
+    topic = columns.Text(required=True)
 
     # State of the item (serialized dict, possibly encrypted).
-    d = columns.Text(required=True)
+    data = columns.Text(required=True)
 
 
-class SnapshotRecord(Model):
+class SnapshotRecord(ActiveRecord):
     """Stores snapshots in Cassandra."""
-
+    __table_name__ = 'snapshots'
     _if_not_exists = True
 
-    __table_name__ = 'snapshots'
-
     # Sequence ID (e.g. an entity or aggregate ID).
-    s = columns.UUID(partition_key=True)
+    sequence_id = columns.UUID(partition_key=True)
 
     # Position (index) of item in sequence.
-    p = columns.BigInt(clustering_order='DESC', primary_key=True)
+    position = columns.BigInt(clustering_order='DESC', primary_key=True)
 
     # Topic of the item (e.g. path to domain entity class).
-    t = columns.Text(required=True)
+    topic = columns.Text(required=True)
 
     # State of the entity (serialized dict, possibly encrypted).
-    d = columns.Text(required=True)
+    data = columns.Text(required=True)
