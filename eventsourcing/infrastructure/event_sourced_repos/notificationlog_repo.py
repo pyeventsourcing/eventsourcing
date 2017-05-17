a7db81052c250b9240d59f6cce149321dad0b1b6
John Bywater <john.bywater@appropriatesoftware.net>
2017-05-17 21:40:36 +0100

    Removed old notification log modules.

--- eventsourcing/infrastructure/event_sourced_repos/notificationlog_repo.py
+++
@@ -1,9 +0,0 @@
-# from eventsourcing.domain.model.notificationlog import NotificationLog, NotificationLogRepository
-# from eventsourcing.infrastructure.eventsourcedrepository import EventSourcedRepository
-#
-#
-# class NotificationLogRepo(EventSourcedRepository, NotificationLogRepository):
-#     """
-#     Event sourced repository for the Example domain model entity.
-#     """
-#     domain_class = NotificationLog
