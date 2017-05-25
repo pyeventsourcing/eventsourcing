e0eba034a2932c4d65bb35181caa41c0ce3f58ab
John Bywater <john.bywater@appropriatesoftware.net>
2017-05-25 23:12:34 +0100

    Changed development status classifier to "Production/Stable".

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     long_description=long_description,
     keywords=['event sourcing', 'event store', 'domain driven design', 'ddd', 'cqrs', 'cqs'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
