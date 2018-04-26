73bc7eb0e06dadb89b25a3d91e857802fb474418
John Bywater <john.bywater@appropriatesoftware.net>
2018-04-26 14:20:40 +0100

    Fixed classifiers.

--- setup.py
+++ setup.py
@@ -99,8 +99,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
-        'Programming Language :: Python :: Implementation :: PyPy3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
