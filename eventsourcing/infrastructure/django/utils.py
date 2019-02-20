de9253c808da67e102265185124babb6d7f05b6e
John Bywater <john.bywater@appropriatesoftware.net>
2019-02-20 01:56:39 +0000

    Removed unused code.

--- eventsourcing/infrastructure/django/utils.py
+++ eventsourcing/infrastructure/django/utils.py
@@ -1,13 +1,6 @@
-import six
-
-
 def close_django_connection():
-    if six.PY2:
-        from django import db
-        db.connections.close_all()
-    else:
-        from django.db import connection
-        connection.close()
+    from django.db import connection
+    connection.close()
 
 
 def setup_django():
