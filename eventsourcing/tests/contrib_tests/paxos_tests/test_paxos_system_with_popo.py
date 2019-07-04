8fa41fb93b2cbb94d2e4aeee16865dd0bd70b24e
John Bywater <john.bywater@appropriatesoftware.net>
2019-07-04 03:10:06 +0100

    Skipped multi-threaded Paxos test with SQLAlchemy on Travis (again).

--- eventsourcing/tests/contrib_tests/paxos_tests/test_paxos_system_with_popo.py
+++ eventsourcing/tests/contrib_tests/paxos_tests/test_paxos_system_with_popo.py
@@ -8,12 +8,6 @@ from eventsourcing.application.popo import PopoApplication
 class TestPaxosSystemWithPopo(PopoTestCase, test_paxos_system.TestPaxosSystem):
     infrastructure_class = PopoApplication
 
-    def test_single_threaded(self):
-        super(TestPaxosSystemWithPopo, self).test_single_threaded()
-        
-    def test_multi_threaded(self):
-        super(TestPaxosSystemWithPopo, self).test_multi_threaded(is_skipped=False)
-
     @skip("Popo doesn't do multiprocessing")
     def test_multiprocessing_performance(self):
         pass
