ac7aeaf347f84d3653f1fd22b6fc109b6e1fffd6
John Bywater <john.bywater@appropriatesoftware.net>
2019-10-16 21:47:33 +0100

    Fixed trouble with dataclass formatting in pypy3.

--- eventsourcing/tests/test_transcoding.py
+++ eventsourcing/tests/test_transcoding.py
@@ -4,7 +4,7 @@ from collections import deque, namedtuple
 import sys
 
 if sys.version_info >= (3, 7):
-    from dataclasses import dataclass
+    from dataclasses import make_dataclass
 
 from decimal import Decimal
 from unittest import TestCase
@@ -198,8 +198,4 @@ MyNamedTuple = namedtuple("MyNamedTuple", field_names=["a", "b", "c"])
 
 
 if sys.version_info >= (3, 7):
-    @dataclass
-    class MyDataClass(object):
-        a: int
-        b: str
-        c: Decimal
+    MyDataClass = make_dataclass('MyDataClass', ['a', 'b', 'c'])
