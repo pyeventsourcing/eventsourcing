d74b55b881cf39a829de1ee0147d565c652ceca8
John Bywater <john.bywater@appropriatesoftware.net>
2019-11-27 22:07:52 +0000

    Reordered imports.

--- eventsourcing/infrastructure/sequenceditem.py
+++ eventsourcing/infrastructure/sequenceditem.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-from typing import Type, Tuple, NamedTuple
+from typing import NamedTuple, Tuple, Type
 
 SequencedItem = namedtuple(
     "SequencedItem", ["sequence_id", "position", "topic", "state"]
