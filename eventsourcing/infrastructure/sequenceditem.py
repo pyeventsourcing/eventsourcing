632e0165b4f0768bb3225a83a0fcca71f78b08f9
John Bywater <john.bywater@appropriatesoftware.net>
2019-11-27 22:01:09 +0000

    Added type hinting to sequenceditem module.

--- eventsourcing/infrastructure/sequenceditem.py
+++ eventsourcing/infrastructure/sequenceditem.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from typing import Type, Tuple, NamedTuple
 
 SequencedItem = namedtuple(
     "SequencedItem", ["sequence_id", "position", "topic", "state"]
@@ -10,32 +11,32 @@ StoredEvent = namedtuple(
 
 
 class SequencedItemFieldNames(object):
-    def __init__(self, sequenced_item_class):
+    def __init__(self, sequenced_item_class: Type[NamedTuple]):
         self._field_names = sequenced_item_class._fields
 
     @property
-    def sequence_id(self):
+    def sequence_id(self) -> str:
         # Sequence ID is assumed to be the first field of a sequenced item.
         return self._field_names[0]
 
     @property
-    def position(self):
+    def position(self) -> str:
         # Position is assumed to be the second field of a sequenced item.
         return self._field_names[1]
 
     @property
-    def topic(self):
+    def topic(self) -> str:
         # Topic is assumed to be the third field of a sequenced item.
         return self._field_names[2]
 
     @property
-    def state(self):
+    def state(self) -> str:
         # State is assumed to be the fourth field of a sequenced item.
         return self._field_names[3]
 
     @property
-    def other_names(self):
+    def other_names(self) -> Tuple[str, ...]:
         return self._field_names[4:]
 
-    def __getitem__(self, i):
+    def __getitem__(self, i: int) -> str:
         return self._field_names[i]
