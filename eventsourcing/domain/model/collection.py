a0e73d5a1bfd30236912ee014234685305a7afd8
John Bywater <john.bywater@appropriatesoftware.net>
2019-11-29 03:18:34 +0000

    Adding more type hinting (collections).

--- eventsourcing/domain/model/collection.py
+++ eventsourcing/domain/model/collection.py
@@ -1,6 +1,12 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from eventsourcing.domain.model.entity import TimestampedVersionedEntity, TTimestampedVersionedEntity
+from typing import Any, Set, Iterator, Optional
+from uuid import UUID
+
+from eventsourcing.domain.model.entity import (
+    TTimestampedVersionedEntity,
+    TimestampedVersionedEntity,
+)
 from eventsourcing.infrastructure.base import AbstractEntityRepository
 
 
@@ -9,48 +15,50 @@ class Collection(TimestampedVersionedEntity):
         """Supertype for events of collection entities."""
 
     class Created(
-        Event[TTimestampedVersionedEntity], TimestampedVersionedEntity.Created[TTimestampedVersionedEntity]
+        Event[TTimestampedVersionedEntity],
+        TimestampedVersionedEntity.Created[TTimestampedVersionedEntity],
     ):
         """Published when collection is created."""
 
     class Discarded(
-        Event[TTimestampedVersionedEntity], TimestampedVersionedEntity.Discarded[TTimestampedVersionedEntity]
+        Event[TTimestampedVersionedEntity],
+        TimestampedVersionedEntity.Discarded[TTimestampedVersionedEntity],
     ):
         """Published when collection is discarded."""
 
     class EventWithItem(Event[TTimestampedVersionedEntity]):
         @property
-        def item(self):
+        def item(self) -> Any:
             return self.__dict__["item"]
+
+    def __init__(self, **kwargs: Any):
+        super(Collection, self).__init__(**kwargs)
+        self._items: Set = set()
+
+    def __iter__(self) -> Iterator:
+        return self._items.__iter__()
+
+    @property
+    def items(self) -> Set:
+        self.__assert_not_discarded__()
+        return self._items
+
+    def add_item(self, item: Any) -> None:
+        self.__trigger_event__(self.ItemAdded, item=item)
 
     class ItemAdded(EventWithItem):
         def mutate(self, obj: "Collection") -> None:
             obj._items.add(self.item)
 
+    def remove_item(self, item: Any) -> None:
+        self.__trigger_event__(self.ItemRemoved, item=item)
+
     class ItemRemoved(EventWithItem):
         def mutate(self, obj: "Collection") -> None:
             obj._items.remove(self.item)
 
-    def __init__(self, **kwargs):
-        super(Collection, self).__init__(**kwargs)
-        self._items = set()
 
-    def __iter__(self):
-        return self._items.__iter__()
-
-    @property
-    def items(self):
-        self.__assert_not_discarded__()
-        return self._items
-
-    def add_item(self, item):
-        self.__trigger_event__(self.ItemAdded, item=item)
-
-    def remove_item(self, item):
-        self.__trigger_event__(self.ItemRemoved, item=item)
-
-
-def register_new_collection(collection_id=None):
+def register_new_collection(collection_id: Optional[UUID] = None) -> Collection:
     return Collection.__create__(originator_id=collection_id)
 
 
