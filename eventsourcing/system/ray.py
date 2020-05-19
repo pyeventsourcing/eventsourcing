caddf1b1381ec056cddca74dd604019a5707f5ca
John Bywater <john.bywater@appropriatesoftware.net>
2020-05-19 15:07:38 +0100

    Removed method RayRunner.call() after changing application interactions interface to work in the same way as other runners:
    runner.get(ApplicationCls, pipeline_id).command_method(arg, kwarg=kwarg)

--- eventsourcing/system/ray.py
+++ eventsourcing/system/ray.py
@@ -135,11 +135,6 @@ class RayRunner(AbstractSystemRunner):
         assert isinstance(process_name, str)
         return self.ray_processes[(process_name, pipeline_id)]
 
-    def call(self, process_name, pipeline_id, method_name, *args, **kwargs):
-        paxosapplication0 = self.get_ray_process(process_name, pipeline_id)
-        ray_id = paxosapplication0.call.remote(method_name, *args, **kwargs)
-        return ray.get(ray_id)
-
     def close(self):
         super(RayRunner, self).close()
         for process in self.ray_processes.values():
