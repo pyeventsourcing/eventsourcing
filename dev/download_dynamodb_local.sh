0057a3f3b5ea617997dbf5218d5b53d6e0224c79
Reggie Melvin <reggie.melvin@neurotrack.com>
2020-06-12 15:59:21 -0700

    fix tar command

--- dev/download_dynamodb_local.sh
+++ dev/download_dynamodb_local.sh
@@ -2,5 +2,5 @@
 mkdir -p dynamodb_local
 wget -O dynamodb_local/dynamodb_local_latest.tar.gz https://s3.us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz
 cd dynamodb_local
-tar -xzf dynamodb_local/dynamodb_local_latest.tar.gz
+tar -xzf dynamodb_local_latest.tar.gz
 rm dynamodb_local_latest.tar.gz
