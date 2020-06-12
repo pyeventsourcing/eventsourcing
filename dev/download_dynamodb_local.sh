c68dc73d3c9094bccd4aaa9d6e3634311c7501f8
Reggie Melvin <reggie.melvin@neurotrack.com>
2020-06-12 15:54:47 -0700

    separated cd command from tar command

--- dev/download_dynamodb_local.sh
+++ dev/download_dynamodb_local.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p dynamodb_local
 wget -O dynamodb_local/dynamodb_local_latest.tar.gz https://s3.us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz
-tar --cd dynamodb_local -xzf dynamodb_local/dynamodb_local_latest.tar.gz
-rm dynamodb_local/dynamodb_local_latest.tar.gz
+cd dynamodb_local
+tar -xzf dynamodb_local/dynamodb_local_latest.tar.gz
+rm dynamodb_local_latest.tar.gz
