4ce67be05015d035bd7fd23db1f0ba0ef58c0aba
Reggie Melvin <reggie.melvin@neurotrack.com>
2020-06-12 16:04:38 -0700

    put execute perms on dynamodb jar

--- dev/download_dynamodb_local.sh
+++ dev/download_dynamodb_local.sh
@@ -3,4 +3,5 @@ mkdir -p dynamodb_local
 wget -O dynamodb_local/dynamodb_local_latest.tar.gz https://s3.us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz
 cd dynamodb_local
 tar -xzf dynamodb_local_latest.tar.gz
+chmod +x DynamoDBLocal.jar
 rm dynamodb_local_latest.tar.gz
