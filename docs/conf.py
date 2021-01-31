426372c0de534948f93b54cf795d0367ce829fd4
John Bywater <john.bywater@appropriatesoftware.net>
2021-01-31 15:35:05 +0000

    Adjusted docs conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -112,8 +112,8 @@ todo_include_todos = False
 html_theme = "python_docs_theme"
 # html_theme = "sphinx_rtd_theme"
 
-import sphinx_rtd_theme
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# import sphinx_rtd_theme
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # html_theme_path = [alabaster.get_html_theme_path()]
 
