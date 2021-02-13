4cae8350ff6a724b33b3d2234b15e1d27f08b93f
John Bywater <john.bywater@appropriatesoftware.net>
2021-02-13 02:28:22 +0000

    Got it working with @event and property setter. :-)

--- eventsourcing/declarative.py
+++ eventsourcing/declarative.py
@@ -1,76 +1,50 @@
 import inspect
+from builtins import property
 from copy import copy
 from dataclasses import dataclass
-from typing import Type, cast
+from types import FunctionType
+from typing import Any, Dict, Type, Union, cast
 from uuid import uuid4
 
 from eventsourcing.domain import Aggregate
 
+original_methods: Dict[Type[Aggregate.Event], FunctionType] = {}
 
-def aggregate(cls) -> Type[Aggregate]:
-    new_cls = type(cls.__name__, (cls, Aggregate), {})
-    new_cls.__qualname__ = cls.__qualname__
-    new_cls.__module__ = cls.__module__
-    new_cls.__doc__ = cls.__doc__
 
-    created_cls = Aggregate.Created
+def aggregate(original_cls: Type[Any]) -> Type[Aggregate]:
 
-    def apply(self, aggregate):
-        event_obj_dict = copy(self.__dict__)
-        event_obj_dict.pop("originator_id")
-        event_obj_dict.pop("originator_version")
-        event_obj_dict.pop("timestamp")
-        self.original_method(aggregate, **event_obj_dict)
+    # Prepare the "created" event class.
+    created_cls_annotations = {}
+    try:
+        cls_init_method = original_cls.__dict__["__init__"]
+    except KeyError:
+        has_init_method = False
+    else:
+        has_init_method = True
+        method_signature = inspect.signature(cls_init_method)
+        for param_name in method_signature.parameters:
+            if param_name == "self":
+                continue
+            created_cls_annotations[param_name] = "typing.Any"
 
-    for name in dir(cls):
-        attribute = getattr(cls, name)
-        if isinstance(attribute, event):
-            # Prepare the subsequent aggregate events.
-            original_method = attribute.original_method
-            event_cls_name = "".join([s.capitalize() for s in name.split("_")])
+    created_cls_dict = {
+        "__annotations__": created_cls_annotations,
+        "__qualname__": ".".join([original_cls.__qualname__, "Created"]),
+        "__module__": original_cls.__module__,
+    }
 
-            event_cls_qualname = ".".join([cls.__qualname__, event_cls_name])
+    created_cls = cast(
+        Type[Aggregate.Created],
+        type("Created", (Aggregate.Created,), created_cls_dict),
+    )
 
-            method_signature = inspect.signature(original_method)
-            annotations = {}
-            for param_name in method_signature.parameters:
-                if param_name == "self":
-                    continue
-                annotations[param_name] = "typing.Any"
+    created_cls = dataclass(frozen=True)(created_cls)
 
-            event_cls_dict = {"__annotations__": annotations}
+    # Prepare the aggregate class.
+    def __new__(cls: Type[Aggregate], **kwargs: Any) -> Aggregate:
+        return cls._create(event_class=created_cls, id=uuid4(), **kwargs)
 
-            event_cls = type(event_cls_name, (Aggregate.Event,), event_cls_dict)
-
-            event_cls = dataclass(event_cls, frozen=True)
-            event_cls.__qualname__ = event_cls_qualname
-            event_cls.__module__ = cls.__module__
-
-            event_cls.apply = apply
-            event_cls.original_method = staticmethod(original_method)
-            setattr(new_cls, event_cls_name, event_cls)
-        elif name == "__init__":
-            # Prepare the "created" event.
-            method_signature = inspect.signature(attribute)
-            annotations = {}
-            for param_name in method_signature.parameters:
-                if param_name == "self":
-                    continue
-                annotations[param_name] = "typing.Any"
-
-            event_cls_dict = {"__annotations__": annotations}
-
-            created_cls = type("Created", (Aggregate.Created,), event_cls_dict)
-            created_cls.__qualname__ = ".".join([cls.__qualname__, "Created"])
-            created_cls.__module__ = cls.__module__
-
-            new_cls.Created = created_cls
-
-            created_cls = dataclass(created_cls, frozen=True)
-
-    original_init = new_cls.__init__
-
-    def __init__(self, **kwargs):
+    def __init__(self: Aggregate, **kwargs: Any) -> None:
         if "id" not in kwargs:
             return  # Python calls me again...
         base_kwargs = {}
@@ -78,18 +52,131 @@ def aggregate(cls) -> Type[Aggregate]:
         base_kwargs["version"] = kwargs.pop("version")
         base_kwargs["timestamp"] = kwargs.pop("timestamp")
         Aggregate.__init__(self, **base_kwargs)
-        original_init(self, **kwargs)
+        if has_init_method:
+            original_cls.__init__(self, **kwargs)
 
-    cls.__init__ = __init__
+    def __getattribute__(self, item):
+        attr = super(Aggregate, self).__getattribute__(item)
+        if isinstance(attr, event):
+            return bound_event(attr, self)
+        else:
+            return attr
 
-    def __new__(cls, **kwargs):
-        return cls._create(event_class=created_cls, id=uuid4(), **kwargs)
+    aggregate_cls_dict = {
+        "__module__": original_cls.__module__,
+        "__qualname__": original_cls.__qualname__,
+        "__doc__": original_cls.__doc__,
+        "__new__": __new__,
+        "__init__": __init__,
+        "__getattribute__": __getattribute__,
+        "Created": created_cls,
+    }
+    aggregate_cls = cast(
+        Aggregate,
+        type(original_cls.__name__, (original_cls, Aggregate), aggregate_cls_dict),
+    )
 
-    new_cls.__new__ = __new__
+    # Prepare the aggregate event classes.
+    def apply(self: Aggregate.Event, aggregate: Aggregate) -> None:
+        event_obj_dict = copy(self.__dict__)
+        event_obj_dict.pop("originator_id")
+        event_obj_dict.pop("originator_version")
+        event_obj_dict.pop("timestamp")
+        original_methods[type(self)](aggregate, **event_obj_dict)
 
-    return cast(Type[Aggregate], new_cls)
+    for name in dir(original_cls):
+        attribute = getattr(original_cls, name)
+        if isinstance(attribute, (event, property)):
+            if isinstance(attribute, property):
+                if isinstance(attribute.fset, event):
+                    attribute = attribute.fset
+                    attribute.is_property_setter = True
+                else:
+                    continue
+
+            # Prepare the subsequent aggregate events.
+            original_method = attribute.original_method
+
+            event_cls_name = attribute.event_cls_name
+            event_cls_qualname = ".".join([original_cls.__qualname__, event_cls_name])
+
+            if isinstance(original_method, property):
+                assert original_method.setter
+                original_method = original_method.fset
+            method_signature = inspect.signature(original_method)
+            annotations = {}
+            for param_name in method_signature.parameters:
+                if param_name == "self":
+                    continue
+                annotations[param_name] = "typing.Any"
+                if attribute.is_property_setter:
+                    attribute.propery_attribute_name = param_name
+                    assert len(method_signature.parameters) == 2
+
+            event_cls_dict = {
+                "__annotations__": annotations,
+                "__module__": original_cls.__module__,
+                "__qualname__": event_cls_qualname,
+                "apply": apply,
+            }
+
+            event_cls: Type[Aggregate.Event] = cast(
+                Type[Aggregate.Event],
+                type(event_cls_name, (Aggregate.Event,), event_cls_dict),
+            )
+
+            event_cls = dataclass(frozen=True)(event_cls)
+
+            original_methods[event_cls] = original_method
+            setattr(aggregate_cls, event_cls_name, event_cls)
+        elif isinstance(attribute, property):
+            if attribute.fset:
+                raise Exception(inspect.signature(attribute.fset))
+
+
+    return cast(Type[Aggregate], aggregate_cls)
 
 
 class event:
-    def __init__(self, original_method):
-        self.original_method = original_method
+    def __init__(self, arg: Union[FunctionType, str]):
+        self.is_property_setter = False
+        self.propery_attribute_name = None
+        assert isinstance(arg, (FunctionType, str))
+        if isinstance(arg, FunctionType):
+            self.original_method = arg
+            name = arg.__name__
+            self.event_cls_name = "".join([s.capitalize() for s in name.split("_")])
+        elif isinstance(arg, str):
+            self.event_cls_name = arg
+            self.original_method = None
+
+    def __call__(self, *args, **kwargs):
+        if self.original_method is None:
+            assert len(kwargs) == 0
+            assert len(args) == 1
+            assert isinstance(args[0], (FunctionType, property))
+            self.original_method = args[0]
+            return self
+        elif len(args) == 2 and len(kwargs) == 0 and isinstance(args[0], Aggregate):
+            # Property assignment?
+            assert self.is_property_setter
+            kwargs = {self.propery_attribute_name: args[1]}
+            bound = bound_event(self, args[0])
+            bound.trigger(**kwargs)
+        else:
+            assert self.event_cls_name is not None
+            assert self.original_method is not None
+            self.original_method(*args, **kwargs)
+
+
+class bound_event:
+    def __init__(self, event: event, aggregate: Aggregate):
+        self.event = event
+        self.aggregate = aggregate
+
+    def trigger(self, **kwargs):
+        event_cls = getattr(self.aggregate, self.event.event_cls_name)
+        self.aggregate._trigger_event(event_cls, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        self.trigger(**kwargs)
--- eventsourcing/tests/test_declarative_syntax.py
+++ eventsourcing/tests/test_declarative_syntax.py
@@ -1,40 +1,20 @@
 from typing import cast
 from unittest import TestCase
+from uuid import UUID
 
 from eventsourcing.application import Application
 from eventsourcing.declarative import aggregate, event
 from eventsourcing.domain import Aggregate
 
 
-@aggregate
-class World:
-    def __init__(self, name):
-        self.name = name
-        self.history = []
-
-    def make_it_so(self, what):
-        self._trigger_event(self.SomethingHappened, what=what)
-
-    @event
-    def _something_happened(self, what):
-        self.history.append(what)
-
-    def set_name(self, name):
-        self._trigger_event(self.NameChanged, name=name)
-
-    @event
-    def _name_changed(self, name):
-        self.name = name
-
-
 class TestDeclarativeSyntax(TestCase):
-    def test_aggregate_and_event(self):
+    def test_world1_aggregate_and_event(self):
 
         # Create a new world.
-        world = World(name="Earth")
+        world = World1(name="Earth")
 
         # Check the aggregate.
-        self.assertIsInstance(world, World)
+        self.assertIsInstance(world, World1)
         self.assertEqual(world.name, "Earth")
         self.assertEqual(world.history, [])
 
@@ -53,37 +33,200 @@ class TestDeclarativeSyntax(TestCase):
         # Check the domain events were triggered.
         pending_events = world.collect_events()
         self.assertEqual(len(pending_events), 3)
-        self.assertIsInstance(pending_events[0], World.Created)
-        self.assertIsInstance(pending_events[1], World.SomethingHappened)
-        self.assertIsInstance(pending_events[2], World.NameChanged)
+        self.assertIsInstance(pending_events[0], World1.Created)
+        self.assertIsInstance(pending_events[1], World1.SomethingHappened)
+        self.assertIsInstance(pending_events[2], World1.NameChanged)
 
-    def test_with_application(self):
+    def test_world1_with_application(self):
 
         # Construct application and aggregate.
         app = Application()
-        world = World(name="Earth")
+        world = World1(name="Earth")
         world.make_it_so("Trucks")
         world.set_name("Mars")
         app.save(cast(Aggregate, world))
 
         # Check the recorded state at current version.
         copy = app.repository.get(world.id)
-        self.assertIsInstance(copy, World)
+        self.assertIsInstance(copy, World1)
         self.assertEqual(copy.name, "Mars")
         self.assertEqual(copy.history, ["Trucks"])
 
         # Check the recorded state at previous versions.
         copy = app.repository.get(world.id, version=1)
-        self.assertIsInstance(copy, World)
+        self.assertIsInstance(copy, World1)
         self.assertEqual(copy.name, "Earth")
         self.assertEqual(copy.history, [])
 
         copy = app.repository.get(world.id, version=2)
-        self.assertIsInstance(copy, World)
+        self.assertIsInstance(copy, World1)
         self.assertEqual(copy.history, ["Trucks"])
         self.assertEqual(copy.name, "Earth")
 
         copy = app.repository.get(world.id, version=3)
-        self.assertIsInstance(copy, World)
+        self.assertIsInstance(copy, World1)
         self.assertEqual(copy.history, ["Trucks"])
         self.assertEqual(copy.name, "Mars")
+
+    def test_world2_aggregate_and_event(self):
+
+        # Create a new world.
+        world = World2(name="Earth")
+
+        # Check the aggregate.
+        self.assertIsInstance(world, World2)
+        self.assertEqual(world.name, "Earth")
+        self.assertEqual(world.history, [])
+
+        # Make trucks so.
+        world.make_it_so("Trucks")
+
+        # Check the history.
+        self.assertEqual(world.history, ["Trucks"])
+
+        # Set the name.
+        world.set_name("Mars")
+
+        # Check the name has changed.
+        self.assertEqual(world.name, "Mars")
+
+        # Check the domain events were triggered.
+        pending_events = world.collect_events()
+        self.assertEqual(len(pending_events), 3)
+        self.assertIsInstance(pending_events[0], World2.Created)
+        self.assertIsInstance(pending_events[1], World2.SomethingHappened)
+        self.assertIsInstance(pending_events[2], World2.NameChanged)
+
+    def test_world2_with_application(self):
+
+        # Construct application and aggregate.
+        app = Application()
+        world = World2(name="Earth")
+        world.make_it_so("Trucks")
+        world.name = "Mars"
+        app.save(cast(Aggregate, world))
+
+        # Check the recorded state at current version.
+        copy = app.repository.get(world.id)
+        self.assertIsInstance(copy, World2)
+        self.assertEqual(copy.name, "Mars")
+        self.assertEqual(copy.history, ["Trucks"])
+
+        # Check the recorded state at previous versions.
+        copy = app.repository.get(world.id, version=1)
+        self.assertIsInstance(copy, World2)
+        self.assertEqual(copy.name, "Earth")
+        self.assertEqual(copy.history, [])
+
+        copy = app.repository.get(world.id, version=2)
+        self.assertIsInstance(copy, World2)
+        self.assertEqual(copy.history, ["Trucks"])
+        self.assertEqual(copy.name, "Earth")
+
+        copy = app.repository.get(world.id, version=3)
+        self.assertIsInstance(copy, World2)
+        self.assertEqual(copy.history, ["Trucks"])
+        self.assertEqual(copy.name, "Mars")
+
+    def test_event_with_event_name(self):
+        world = World2(name="Earth")
+        self.assertEqual(world._a, 1)
+        world.set_a(a=2)
+        self.assertEqual(world._a, 2)
+        self.assertIsInstance(world._pending_events[-1],  World2.AUpdated)
+
+    def test_world3_set_name(self):
+
+        # Create a new world.
+        world = World3(name="Earth")
+        self.assertEqual(world.name, "Earth")
+        world.name = "Mars"
+        self.assertEqual(world.name, "Mars")
+        self.assertIsInstance(world._pending_events[-1],  World3.NameChanged)
+
+    def test_missing_init(self):
+        aggregate = AggregateWithoutInit()
+        self.assertIsInstance(aggregate.id, UUID)
+        aggregate.set_name("name")
+        self.assertEqual(aggregate.name, "name")
+
+
+
+#
+# @aggregate
+# class World1:
+#     def __init__(self, name):
+#         self.name = name
+#         self.history = []
+#
+#     def make_it_so(self, what):
+#         self._trigger_event(self.SomethingHappened, what=what)
+#
+#     @event
+#     def _something_happened(self, what):
+#         self.history.append(what)
+#
+#     def set_name(self, name):
+#         self._trigger_event(self.NameChanged, name=name)
+#
+#     @event
+#     def _name_changed(self, name):
+#         self.name = name
+#
+#
+# @aggregate
+# class World2(Aggregate):
+#
+#     def __init__(self, name):
+#         self._name = name
+#         self._a = 1
+#         self.history = []
+#
+#     @event("AUpdated")
+#     def set_a(self, a):
+#         self._a = a
+#
+#     @property
+#     def name(self):
+#         return self._name
+#
+#     @event
+#     def name_changed(self, name):
+#         self._name = name
+#
+#     @name.setter
+#     def name(self, name):
+#         self.name_changed.trigger(name=name)
+#
+#     @event
+#     def something_happened(self, what):
+#         self.history.append(what)
+#
+#     def make_it_so(self, what):
+#         self.something_happened(what=what)
+
+@aggregate
+class World3:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    @event("NameChanged")
+    def name(self, name):
+        self._name = name
+
+
+
+# @aggregate
+# class AggregateWithoutInit(Aggregate):
+#     @event
+#     def name_changed(self, name):
+#         self.name = name
+#
+#     def set_name(self, name):
+#         self._trigger_event(self.NameChanged, name=name)
+
