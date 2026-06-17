e3f4913ed3c86c3941906a50e9b8aa38a33c390d
johnbywater <john.bywater@appropriatesoftware.net>
2026-06-17 14:08:32 +0100

    Reworked the extended singledispatchmethod implementation to fit with type checking for Python 3.11.

--- eventsourcing/dispatch.py
+++ eventsourcing/dispatch.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
 import functools
+import types
+from collections.abc import Callable
+from threading import Lock
 from typing import TYPE_CHECKING, Any, Generic, TypeVar, cast, overload
 
 _T = TypeVar("_T")
 _S = TypeVar("_S")
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
 
     class _singledispatchmethod(functools.singledispatchmethod[_T]):  # noqa: N801
         pass
@@ -20,61 +22,50 @@ else:
         pass
 
 
+_RegType = type[Any] | types.UnionType
+
+
 class singledispatchmethod(_singledispatchmethod[_T]):  # noqa: N801
     def __init__(self, func: Callable[..., _T]) -> None:
         super().__init__(func)
-        self.deferred_registrations: list[
-            tuple[type[Any] | Callable[..., _T], Callable[..., _T] | None]
-        ] = []
+        self.deferred_registrations_lock = Lock()
+        self.deferred_registrations: list[Callable[..., _T]] = []
 
     @overload
     def register(
-        self, cls: type[Any], method: None = None
-    ) -> Callable[[Callable[..., _T]], Callable[..., _T]]: ...  # pragma: no cover
+        self, cls: _RegType, method: None = None
+    ) -> Callable[[Callable[..., _T]], Callable[..., _T]]: ...
     @overload
     def register(
         self, cls: Callable[..., _T], method: None = None
-    ) -> Callable[..., _T]: ...  # pragma: no cover
-
+    ) -> Callable[..., _T]: ...
     @overload
     def register(
-        self, cls: type[Any], method: Callable[..., _T]
-    ) -> Callable[..., _T]: ...  # pragma: no cover
-
+        self, cls: _RegType, method: Callable[..., _T]
+    ) -> Callable[..., _T]: ...
     def register(
         self,
-        cls: type[Any] | Callable[..., _T],
+        cls: _RegType | Callable[..., _T],
         method: Callable[..., _T] | None = None,
     ) -> Callable[[Callable[..., _T]], Callable[..., _T]] | Callable[..., _T]:
         """generic_method.register(cls, func) -> func
 
         Registers a new implementation for the given *cls* on a *generic_method*.
         """
-        if isinstance(cls, (classmethod, staticmethod)):
-            first_annotation = {}
-            for k, v in cls.__func__.__annotations__.items():
-                first_annotation[k] = v
-                break
-            cls.__annotations__ = first_annotation
-
-            # for globals in typing.get_type_hints() in Python 3.8 and 3.9
-            if not hasattr(cls, "__wrapped__"):
-                cls.__dict__["__wrapped__"] = cls.__func__
-                # cls.__wrapped__ = cls.__func__
-
         try:
-            return self.dispatcher.register(cast("type[Any]", cls), func=method)
+            if method is None:
+                return super().register(cls)
+            return super().register(cast(_RegType, cls), method)
         except (NameError, TypeError):  # NameError <= Py3.13, TypeError >= Py3.14
-            self.deferred_registrations.append(
-                (cls, method)  # pyright: ignore [reportArgumentType]
-            )
-            # TODO: Fix this....
-            return method or cls  # pyright: ignore [reportReturnType]
+            if isinstance(cls, (staticmethod, types.FunctionType)) and method is None:
+                self.deferred_registrations.append(cast(Callable[..., _T], cls))
+                return cast(Callable[..., _T], cls)
+            raise
 
     def __get__(self, obj: _S, cls: type[_S] | None = None) -> Callable[..., _T]:
-        for registered_cls, registered_method in self.deferred_registrations:
-            self.dispatcher.register(
-                cast("type[Any]", registered_cls), func=registered_method
-            )
-        self.deferred_registrations = []
+        if self.deferred_registrations:
+            with self.deferred_registrations_lock:
+                for deferred_callable in self.deferred_registrations:
+                    super().register(deferred_callable)
+                self.deferred_registrations = []
         return super().__get__(obj, cls=cls)
