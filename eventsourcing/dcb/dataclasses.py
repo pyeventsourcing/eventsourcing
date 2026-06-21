3c556fea4061585fabe9dec89fe2314b1085a488
johnbywater <john.bywater@appropriatesoftware.net>
2026-06-21 02:56:11 +0100

    Added eventsourcing.dcb.dataclasses.

---
+++ eventsourcing/dcb/dataclasses.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+from typing import Any
+
+from eventsourcing.dcb import domain
+from eventsourcing.domain import get_metadata_from_context
+
+
+@dataclass(kw_only=True)
+class Decision(domain.Decision):
+    metadata: dict[str, str] = field(default_factory=get_metadata_from_context)
+
+    def as_dict(self) -> dict[str, Any]:
+        return self.__dict__.copy()
+
+
+@dataclass
+class InitialDecision(domain.InitialDecision, Decision):
+    originator_topic: str
